bda98e49084285a5cdab51841deeae6fc8b4b389
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-08-13 17:18:20 +0530

    Post adding form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostCreate from './PostCreate'
+
+export default()=>{
+    return( 
+    <div> 
+        <h1>Post Creation</h1>
+    <PostCreate/>
+    </div>
+    )
+
+}
---
+++ src/PostCreate.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default()=>{
+    return <div>
+        <form >
+            <div class="form-group">
+                <label>
+                    Title
+                </label>
+                <input className="form-control"/>
+            </div>
+
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    </div>
+}
