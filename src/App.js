30132fee3b19b07624e0d5d7edf033f23e0ed316
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-08-14 08:11:24 +0530

    Added cors to the backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PostCreate from './PostCreate'
 
-export default()=>{
-    return( 
-    <div> 
-        <h1>Post Creation</h1>
-    <PostCreate/>
-    </div>
+export default () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-3">
+                    <h1>Post Creation</h1>
+                    <PostCreate />
+                </div>
+            </div>
+        </div>
     )
-
 }
--- src/PostCreate.js
+++ src/PostCreate.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
 
-export default()=>{
-    return <div>
-        <form >
+export default () => {
+
+    const [title, setTitle] = useState('');
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        await Axios.post("http://localhost:4000/posts", {
+            title
+        });
+
+        setTitle('');
+    }
+
+    return <div >
+        <form onSubmit={onSubmit}> 
             <div class="form-group">
                 <label>
                     Title
                 </label>
-                <input className="form-control"/>
+                <input value={title} onChange={e => setTitle(e.target.value)} className="form-control" />
             </div>
 
             <button className="btn btn-primary">Submit</button>
