54f68962ffec31756f805848b08c91d284503bf2
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-08-15 14:02:02 +0530

    Added the query service

--- src/PostList.js
+++ src/PostList.js
@@ -8,7 +8,9 @@ export default () => {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const res = await Axios.get("http://localhost:4000/posts");
+        // const res = await Axios.get("http://localhost:4000/posts");
+        const res = await Axios.get("http://localhost:4002/posts");
+        console.log(res.data);
         setPosts(res.data)
     }
     
@@ -18,15 +20,14 @@ export default () => {
     // fetchPosts();
 
     const renderedPosts = Object.values(posts).map(post => {
-        return <div className="card col-md-4" key={post.id}>
+        return <div className="card col-md-4" key={post.post.id}>
+            {/* {post} */}
             <div className="card-body">
-                {post.title}
+                {post.post.content}
             </div>
-            <CommentList post={post.id}></CommentList>
-            <CommentCreate post={post.id}></CommentCreate>
+            <CommentList post={post.post.id} comments={post.post.comments}></CommentList>
+            <CommentCreate post={post.post.id}></CommentCreate>
         </div>
     })
-
     return renderedPosts
-    
 }
